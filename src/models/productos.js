7f9d1a6c9e3ab037f63b243fd6adcbe17f29fee6
Wili Delgado <wilsondelgadomoreno@gmail.com>
2025-05-31 23:20:20 -0500

    validamso los errores en caso de nos salga error saber el motivo

--- src/models/productos.js
+++ src/models/productos.js
@@ -13,9 +13,15 @@ class Productos {
             const [rows] = await connection.query("SELECT  * FROM  productos WHERE id_producto = ? ",[id]);
             if (rows.affectedRows === 0) {
                 const err  = new Error("Producto No Encontrado")
+                err.status(404);
+                throw err;
             }
+            return rows[0];
         } catch (error) {
-            
+            if (error.status === 404) {
+                throw error 
+            };
+            throw new Error(" Error al  obtener  Productos por id :");
         }
     }
 }
