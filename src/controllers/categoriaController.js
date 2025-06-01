6e392837e7f4db6f884ff1be203181320ac2458f
Wili Delgado <wilsondelgadomoreno@gmail.com>
2025-06-01 03:00:24 -0500

    primer controlado hecho de categorias

--- src/controllers/categoriaController.js
+++ src/controllers/categoriaController.js
@@ -1,6 +1,14 @@
-class CategoriaController {
-    static getAllCategoria = async (req,res) =>{
+import Categorias from "../models/categoria";
 
+class CategoriaController {
+    static getAllCategorias = async (req,res) =>{
+        try {
+            const categorias = new Categorias();
+            const resultado = await categorias.getAllCategorias();
+            res.json(resultado);
+        } catch (error) {
+            res.status(500).json({error:error.message})
+        }
     }
 }
 
