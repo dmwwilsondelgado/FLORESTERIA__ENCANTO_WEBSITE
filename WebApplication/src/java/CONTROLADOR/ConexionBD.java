876e1e7398e91923eedb02a4830e61cb73747ddc
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2025-06-16 10:27:23 -0500

    conexion ala base de datos

--- WebApplication/src/java/CONTROLADOR/ConexionBD.java
+++ WebApplication/src/java/CONTROLADOR/ConexionBD.java
@@ -8,6 +8,22 @@ package CONTROLADOR;
  *
  * @author Propietario
  */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class ConexionBD {
+    public static  String url = "jdbc:mysql//localhost:/floresteria_encanto_database";
+    public static  String user= "wilsondelgado";
+    public static String password= "1102717619";
     
+    public static Connection conectar_metodo(){
+        Connection conexion = null; 
+        try{
+           Class.forName("com.mysql.cj.jdbc.Driver");
+           conexion = DriverManager.getConnection(url, user, password);
+        }catch(ClassNotFoundException | SQLException e){
+            System.out.println("Erorr no se pudo conectar ala base de datos"+e.getMessage());
+        }
+        return conexion;
+    }
 }
