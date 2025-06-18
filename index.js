0d500abebc7f9ff173a553c6fd5434be4ae388aa
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2025-06-18 09:51:23 -0500

    reesturturando el proyecto

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-import express from 'express';
-import usuariosRoutes from './src/routes/usuariosRouter.js';
-import productosRoutes from './src/routes/productosRouter.js';
-import bodyParser from 'body-parser';
+import express from "express";
+import usuariosRoutes from "./src/routes/usuariosRouter.js";
+import productosRoutes from "./src/routes/productosRouter.js";
+import bodyParser from "body-parser";
 const { json } = bodyParser;
-
 
 const app = express();
 
@@ -11,21 +10,25 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // sin esto naaa  para recibir formualrios activos
 
+app.use("/usuarios", usuariosRoutes);
+app.use("/productos", productosRoutes);
 
-app.use('/usuarios', usuariosRoutes); 
-app.use('/productos',productosRoutes)
+const url = fetch("");
 
-
-
-app.listen(3000, () => 
-{
-console.log(`Servidor corriendo en: http://localhost:3000`);
-console.log("   .--.  ");
-console.log("  |o_o | ");
-console.log("  | :/ | ");
-console.log(" //    |Hi developer im  Love for Linux");
-console.log("(:|    |::::::)");
-console.log("/'//_   _/`");
-console.log("\\___)=(___/");
-},
-);
+app.listen(3000, () => {
+  console.log(`Servidor corriendo en: http://localhost:3000`);
+  console.log("   .--.  ");
+  console.log("  |o_o | ");
+  console.log("  | :/ | ");
+  console.log(" //    |Hi developer im  Love for Linux");
+  console.log("(:|    |::::::)");
+  console.log("/'//_   _/`");
+  console.log("\\___)=(___/");
+});
+// enlaze para JAVA
+fetch("http://localhost:8080/floresteria_el_encanto/api/usuarios")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("Usuarios:", data);
+  })
+  .catch((error) => console.error("Error:", error));
